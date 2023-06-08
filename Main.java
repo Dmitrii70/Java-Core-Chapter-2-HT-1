947ffa0cdf8d701831363d8cb572bb2a5aa5d55a
Dmitrii70 <132160315+Dmitrii70@users.noreply.github.com>
2023-06-09 00:57:18 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package JavaCore.Chapter2.Hometask.HT1;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> finalList = new ArrayList<>();
+        for (int i = 0; i < intList.size(); i++) {
+            if ((intList.get(i) > 0) && (intList.get(i) % 2 == 0)) {
+                finalList.add(intList.get(i));
+            }
+        }
+        finalList.sort(Comparator.naturalOrder());
+        System.out.println(finalList);
+    }
+}
