105b6fea8462364aecbca5177df310f4c5760559
Dmitrii70 <132160315+Dmitrii70@users.noreply.github.com>
2023-06-15 23:37:35 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         List<Integer> finalList = new ArrayList<>();
-        for (int i = 0; i < intList.size(); i++) {
-            if ((intList.get(i) > 0) && (intList.get(i) % 2 == 0)) {
-                finalList.add(intList.get(i));
+        for (Integer integer : intList) {
+            if ((integer > 0) && (integer % 2 == 0)) {
+                finalList.add(integer);
             }
         }
         finalList.sort(Comparator.naturalOrder());
